49bd64b97a042ac038ef41a5d9aacd964580bace
vamsidhar2mp <vamsidhar2mp@gmail.com>
2017-09-05 23:34:34 +0530

    added new project(ClimateApp) to work with API's

---
+++ ClimateApp/app/src/main/java/elnino/climateapp/MainController.java
@@ -0,0 +1,13 @@
+package elnino.climateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
